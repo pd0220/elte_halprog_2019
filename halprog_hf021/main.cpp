bb5cc324d996f14aedd2b9bf02b5508ee813855c
pd0220 <41996980+pd0220@users.noreply.github.com>
2019-03-04 12:22:12 +0100

    Add files via upload

--- halprog_hf021/main.cpp
+++ halprog_hf021/main.cpp
@@ -3,7 +3,7 @@
 
 //Newton's method to find the root of a given function
 template<typename F,typename dF,typename CheckF,typename T>
-double Newton(F f,dF df,T x0,CheckF checkf)
+T Newton(F f,dF df,T x0,CheckF checkf)
 {
     //the result will be stored in this variable
     T xi;
@@ -35,5 +35,5 @@ int main(int, char**)
     //result
     //squre root of 612.0
     double result=Newton([](double x){return x*x-612;},[](double x){return 2.0*x;},10.0,check_iteration);
-    std::cout<<result<<std::endl; 
+    std::cout<<result<<std::endl;
 }
