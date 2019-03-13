388ade170878d353477ee221b6432728aab06dc8
pd0220 <41996980+pd0220@users.noreply.github.com>
2019-03-13 14:25:36 +0100

    Add files via upload

--- halprog_hf031/Fit.h
+++ halprog_hf031/Fit.h
@@ -31,12 +31,13 @@ std::array<double,2> linear_fit(const std::vector<double>& X,const std::vector<d
         exit(-2);
     }
     //check if the given vectors have enough elements to fit
-    if(static_cast<int>(X.size())==1 || static_cast<int>(Y.size())==1)
+    if(static_cast<int>(X.size())<=1)
     {
         std::cout<<"Error\nLinear fit cannot be made through one point."<<std::endl;
         exit(-3);
     }
     //check if X vector has at least two identical elements
+    /*
     std::vector<double> X_sort;
     X_sort.assign(X.begin(),X.end());
     std::sort(X_sort.begin(),X_sort.end());
@@ -48,36 +49,22 @@ std::array<double,2> linear_fit(const std::vector<double>& X,const std::vector<d
             exit(-5);            
         }
     }
-
+    */
 
     //means and difference vectors
     double mean_X=mean(X);
     double mean_Y=mean(Y);
-    std::vector<double> X_alt, Y_alt;
-
-    //allocating space
-    X_alt.resize(X.size());
-    Y_alt.resize(Y.size());
-
-    //setting differece vectors via calculated means of vectors
-    std::transform(X.begin(),X.end(),X_alt.begin(),[mean_X](double x){return x-mean_X;});
-    std::transform(Y.begin(),Y.end(),Y_alt.begin(),[mean_Y](double y){return y-mean_Y;});
-
-    //multiply vectors X and Y with each other and summing up the result
-    double b1=std::inner_product(X_alt.begin(),X_alt.end(),Y_alt.begin(),0.0);
-    //multiply the modified vector X with itself and summing up the result
-    double b2=std::inner_product(X_alt.begin(),X_alt.end(),X_alt.begin(),0.0);
 
     //calculating slope
+    double b1=std::inner_product(X.begin(),X.end(),Y.begin(),0.0,[](double a,double b){return a+b;},[mean_X,mean_Y](double a,double b){return (a-mean_X)*(b-mean_Y);});
+    double b2=std::inner_product(X.begin(),X.end(),X.begin(),0.0,[](double a,double b){return a+b;},[mean_X](double a,double b){return (a-mean_X)*(b-mean_X);});
     double b=b1/b2;
 
     //calculating y-intercept
     double m=mean_Y-b*mean_X;
 
     //result array
-    std::array<double,2> result={b,m};
-
-    return result;
+    return std::array<double,2> {b,m};
 }
 
 //r^2 function
@@ -90,7 +77,7 @@ double r_squared(const std::vector<double>& X,const std::vector<double>& Y)
         exit(-2);
     }
     //check if the given vectors have enough elements to fit
-    if(static_cast<int>(X.size())==1 || static_cast<int>(Y.size())==1)
+    if(static_cast<int>(X.size())<=1)
     {
         std::cout<<"Error\nLinear fit cannot be made through one point."<<std::endl;
         exit(-3);
@@ -102,6 +89,7 @@ double r_squared(const std::vector<double>& X,const std::vector<double>& Y)
         exit(-4);
     }
     //check if X vector has at least two identical elements
+    /*
     std::vector<double> X_sort;
     X_sort.assign(X.begin(),X.end());
     std::sort(X_sort.begin(),X_sort.end());
@@ -113,23 +101,18 @@ double r_squared(const std::vector<double>& X,const std::vector<double>& Y)
             exit(-5);            
         }
     }
-
+    */
 
     //means of values
     double mean_X=mean(X);
     double mean_Y=mean(Y);
 
+    //calculating square of given object
+    auto sq=[](auto x){return x*x;};
+
     //means of squared values
-    std::vector<double> X2,Y2;
-    //allocating space
-    X2.resize(X.size());
-    Y2.resize(Y.size());
-    //setting the vectors of squared values
-    std::transform(X.begin(),X.end(),X2.begin(),[](double x){return std::pow(x,2);});
-    std::transform(Y.begin(),Y.end(),Y2.begin(),[](double y){return std::pow(y,2);});
-    //calculating means
-    double mean_X2=mean(X2);
-    double mean_Y2=mean(Y2);
+    double mean_X2=std::accumulate(X.begin(),X.end(),0.0,[sq](double a,double b){return a+sq(b);})/static_cast<double>(X.size());
+    double mean_Y2=std::accumulate(Y.begin(),Y.end(),0.0,[sq](double a,double b){return a+sq(b);})/static_cast<double>(Y.size());
 
     //mixed mean value
     double mean_X_mean_Y=mean_X*mean_Y;
@@ -138,7 +121,7 @@ double r_squared(const std::vector<double>& X,const std::vector<double>& Y)
     double mean_XY=std::inner_product(X.begin(),X.end(),Y.begin(),0.0)/static_cast<double>(X.size());;
 
     //calculating r^2
-    double r_sq=std::pow((mean_XY-mean_X_mean_Y)/std::sqrt((mean_X2-std::pow(mean_X,2))*(mean_Y2-std::pow(mean_Y,2))),2);
+    double r_sq=sq((mean_XY-mean_X_mean_Y)/std::sqrt((mean_X2-sq(mean_X))*(mean_Y2-sq(mean_Y))));
     
     return r_sq;
 }
--- halprog_hf031/main.cpp
+++ halprog_hf031/main.cpp
@@ -4,14 +4,14 @@
 int main(int, char**)
 {
     //given vectors
-    const std::vector<double> X={1.0,2.0,3.0}, Y={2.0,1.8,2.9};
+    const std::vector<double> X={1.0,2.0,3.0}, Y={1.0,1.8,2.9};
     
     //linear fit
     std::array<double,2> result=linear_fit(X,Y);
 
     //r^2
     double r_sq=r_squared(X,Y);
-
+    
     //printing the equation of the fitted linear function
     std::cout<<"Equation of the fitted linear function:\ny(x) = "<<result[0]<<"*x + "<<result[1]<<std::endl;
     std::cout<<"Value of r^2 is: "<<r_sq<<std::endl;
