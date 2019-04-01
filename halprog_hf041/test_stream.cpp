bf2a539c7187c72758ffae07e8b2d4e6aed7532f
pd0220 <41996980+pd0220@users.noreply.github.com>
2019-04-01 13:18:51 +0200

    Add files via upload

--- halprog_hf041/test_stream.cpp
+++ halprog_hf041/test_stream.cpp
@@ -4,16 +4,44 @@
 //main function
 int main(int,char**)
 {
-    //test vector will be v1=(1.5,2.5)
-    vector2<double> v1{1.5,2.5};
-    std::stringstream ss1;
-    ss1<<v1;
+    //test vector will be v=(1.5,2.5)
+    vector2<double> v{1.5,2.5};
+    std::stringstream ss;
+    ss<<v;
 
-    vector2<double> v2;
-    ss1>>v2;
+    vector2<double> w;
+    ss>>w;
 
-    if(v1.x!=v2.x || v1.y!=v2.y)
+    if(v.x!=w.x || v.y!=w.y)
     {
+        return 1;
+    }
+
+    //test string will be str1("1.1,2.2")
+    //test vector will x_ref=(1.1,2.2)
+    std::string str("1.1,2.2");
+    vector2<double> x_ref{1.1,2.2};
+    std::stringstream ss_1;
+    ss_1<<str;
+
+    vector2<double> x;
+    ss_1>>x;
+
+    if(x_ref.x!=x.x || x_ref.y!=x.y)
+    {
+        return 1;
+    }
+
+    //test vector will be y=(3.14,42.1)
+    //test string will be str_ref("3.14,42.1")
+    vector2<double> y{3.14,42.1};
+    std::stringstream ssy;
+    ssy<<y;
+    std::string str_ref("3.14,42.1");
+
+    if(str_ref!=ssy.str())
+    {
+        std::cout<<"prob"<<std::endl;
         return 1;
     }
 
